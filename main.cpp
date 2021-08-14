ac4687c0d8c868f129f28653d56f48b502a83ffa
0ptim0 <optim000000@gmail.com>
2021-08-14 19:46:17 +0700

    Party with containers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include "stm32_conf.h"
+#include <array>
+#include <algorithm>
+#include <cstdint>
+
+namespace {
+    std::array<std::uint8_t, 4U> counters;
+}
+
+void blah() {
+    std::fill(counters.begin(), 
+                counters.end(), 
+                    static_cast<std::uint8_t>(1U));
+}
 
 int main(void) {
+    blah();
     while(1) {
     }
 }
